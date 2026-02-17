6355e46791eaa4a02570b56845d4777d333af163
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-02-17 18:26:59 +0100

    Pridani automapper
    Přidání a nastavení automapperu

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/AutoMapper/AutoMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using AvilokTaskAssignment.Data.Models;
+using AvilokTaskAssignment.Api.DTO;
+using Microsoft.Identity.Client;
+
+namespace AvilokTaskAssignment.Api.AutoMapper
+{
+    public class AutoMappingProfile: Profile
+    {
+        public AutoMappingProfile() 
+        {
+            CreateMap<CreateTaskDto, CreateTaskDto>();
+        }
+    }
+}
