373186d70a328ab49bdcdbd1a7ba553c33723847
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-02-19 17:29:56 +0100

    AuthController
    Vytvoření AuthController a metody register

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AvilokTaskAssignment.Api.Managers;
+using AvilokTaskAssignment.Api.DTO;
+
+namespace AvilokTaskAssignment.Api.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthManager _authManager;
+
+        public AuthController(AuthManager authManager)
+        {
+            _authManager = authManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserDto registerUserDto)
+        {
+            await _authManager.RegisterAsync(registerUserDto);
+            return Ok("Uživatel vytvořen.");
+        }
+    }
+}
