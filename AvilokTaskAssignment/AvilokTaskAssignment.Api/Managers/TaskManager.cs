91148fd1a30989dccd6f97c3ad5a22dd5b9eb233
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-03-11 13:04:54 +0100

    Pridani metody GetFiltredTasks
    Přidání metody GetFiltredTask do TaskManager

--- AvilokTaskAssignment/AvilokTaskAssignment.Api/Managers/TaskManager.cs
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/Managers/TaskManager.cs
@@ -1,6 +1,38 @@
-﻿namespace AvilokTaskAssignment.Api.Managers
+﻿using AutoMapper;
+using AvilokTaskAssignment.Data.Interfaces;
+using AvilokTaskAssignment.Data.Models;
+using AvilokTaskAssignment.Api.DTO;
+
+namespace AvilokTaskAssignment.Api.Managers
 {
     public class TaskManager
     {
+        private readonly ITaskRepository _taskRepository;
+        private readonly IMapper _mapper;
+
+        public TaskManager(ITaskRepository taskRepository, IMapper mapper)
+        {
+            _taskRepository = taskRepository;
+            _mapper = mapper;
+        }
+
+        #region Get
+
+        /// <summary>
+        /// Vratí filtrovaný seznam zakázek.
+        /// </summary>
+        public async Task<IEnumerable<TaskListDto>> GetFilteredTasksAsync(
+            WorkType? workType,
+            Guid? createdById,
+            Guid? assignedUserId,
+            AvilokTaskAssignment.Data.Models.TaskStatus? status)
+
+        {
+            var tasks = await _taskRepository.GetFilteredAsync(workType, createdById, assignedUserId, status);
+
+            return _mapper.Map<IEnumerable<TaskListDto>>(tasks);
+        }
+        #endregion
+
     }
 }
