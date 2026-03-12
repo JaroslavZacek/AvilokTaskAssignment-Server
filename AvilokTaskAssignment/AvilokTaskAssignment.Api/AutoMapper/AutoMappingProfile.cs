833a30b85db141edf796c814d57cf793f805fd8c
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-03-12 07:52:05 +0100

    Upraven AutoMapper
    Úprava automapper, kvůli přidání nové createMap pro nové Dto

--- AvilokTaskAssignment/AvilokTaskAssignment.Api/AutoMapper/AutoMappingProfile.cs
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/AutoMapper/AutoMappingProfile.cs
@@ -10,6 +10,13 @@ namespace AvilokTaskAssignment.Api.AutoMapper
         public AutoMappingProfile() 
         {
             CreateMap<CreateTaskDto, CreateTaskDto>();
+
+            CreateMap<TaskItem, TaskListDto>()
+                .ForMember(dest => dest.CreatedByName,
+                    opt => opt.MapFrom(src => src.CreatedBy.UserName))
+                .ForMember(dest => dest.AssignedUserName,
+                    opt => opt.MapFrom(src => 
+                        src.Status != Data.Models.TaskStatus.Closed && src.Deadline < DateTime.UtcNow));
         }
     }
 }
