ddaa299115083bae15660f28b5f19d85cda347fb
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-10 14:09:49 +0200

    Pridana CurrentUserDto
    Přidána CurrentUserDto lepší odesílání clientovy

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/DTO/CurrentUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AvilokTaskAssignment.Api.DTO
+{
+    public class CurrentUserDto
+    {
+        public string UserId { get; set; } = string.Empty;
+
+        public string? Email { get; set; }
+
+        public List<string> Roles { get; set; } = [];
+    }
+}
