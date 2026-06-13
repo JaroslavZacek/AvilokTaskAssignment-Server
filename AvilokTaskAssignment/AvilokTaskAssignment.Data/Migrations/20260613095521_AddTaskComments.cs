50816a2940c7b4cbb602d7306f36f265f4cf2655
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-13 12:03:23 +0200

    Create AddTaskComments Migration
    Přidána migrace pro komentáře

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Data/Migrations/20260613095521_AddTaskComments.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AvilokTaskAssignment.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskComents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TaskId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskComents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskComents_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskComents_TaskItems_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "TaskItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskComents_AuthorId",
+                table: "TaskComents",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskComents_TaskId",
+                table: "TaskComents",
+                column: "TaskId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskComents");
+        }
+    }
+}
