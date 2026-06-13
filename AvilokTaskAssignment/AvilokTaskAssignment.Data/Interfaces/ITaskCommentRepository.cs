9dd34fb0d62092b12c0ca511bab2ee7fdcfc2bb6
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-13 10:57:57 +0200

    Create ITaskCommentRepository.cs
    Vytvořeno rozhraní ITaskCommentRepository.cs

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Data/Interfaces/ITaskCommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AvilokTaskAssignment.Data.Models;
+
+namespace AvilokTaskAssignment.Data.Interfaces
+{
+    public interface ITaskCommentRepository : IBaseRepository<TaskComment>
+    {
+        Task<IEnumerable<TaskComment>> GetByTaskIdAsync(Guid taskId);
+    }
+}
