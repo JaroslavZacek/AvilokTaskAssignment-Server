13c944df17714f0053a8417243861d6561e6707d
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-13 20:29:02 +0200

    Create WorkTypeExtensions.cs
    Vytvořená pomocná class WorkTypeExtensions.cs, kde se uchovávají pomocné metody pro získání jmen rolí, kvůli kontrolám jestli metodu může použít někdo s danou rolí.

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/Helpers/WorkTypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using AvilokTaskAssignment.Data.Models;
+
+namespace AvilokTaskAssignment.Api.Helpers
+{
+    public static class WorkTypeExtensions
+    {
+        public static string GetRoleName(this WorkType workType)
+        {
+            return workType switch
+            {
+                WorkType.Developer => "Developer",
+                WorkType.GraphicDesigner => "Graphic",
+                WorkType.Storyteller => "Story",
+                _ => throw new Exception("Neznámí typ práce.")
+            };
+        }
+
+        public static string GetLeaderRoleName (this WorkType workType)
+        {
+            return $"Leader {workType.GetRoleName()}";
+        }
+    }
+}
