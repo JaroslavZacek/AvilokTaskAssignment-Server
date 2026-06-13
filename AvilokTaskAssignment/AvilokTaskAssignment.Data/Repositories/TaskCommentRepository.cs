ce4812c61464fe8e6800594051690a1cd1822c8f
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-13 10:55:42 +0200

    Create TaskCommentRepository.cs
    Vytvořeno TaskCommentRepository.cs

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Data/Repositories/TaskCommentRepository.cs
@@ -0,0 +1,27 @@
+﻿using AvilokTaskAssignment.Data.Models;
+using AvilokTaskAssignment.Data.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AvilokTaskAssignment.Data.Repositories
+{
+    public class TaskCommentRepository : BaseRepository<TaskComment>
+    {
+        public TaskCommentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<TaskComment>> GetByTaskIdAsync(Guid taskId)
+        {
+            return await _context.TaskComents
+                .Include(c => c.Author)
+                .Where(c => c.TaskId == taskId)
+                .OrderBy(c => c.CreatedAt)
+                .ToListAsync();
+        }
+    }
+}
