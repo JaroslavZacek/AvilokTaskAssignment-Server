aae73ddafbc306df888d8c6ae8a361b6b2b1a740
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-13 10:43:07 +0200

    Create CommentDto.cs
    Vytvoření dto pro odesílání informací komentáře

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/DTO/CommentDto.cs
@@ -0,0 +1,17 @@
+﻿namespace AvilokTaskAssignment.Api.DTO
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+
+        // ----------------------------
+        // Informace o Autorovi komentáře
+        // ----------------------------
+        public Guid AuthorId { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
