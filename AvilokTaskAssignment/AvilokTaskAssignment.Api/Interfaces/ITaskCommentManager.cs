4d8a99b37cb93a4777ee87b66bd452cfb82dadac
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-13 11:04:24 +0200

    Create ITaskCommentManager.cs
    Vytvoření rozhraní ITaskCommentManager.cs

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/Interfaces/ITaskCommentManager.cs
@@ -0,0 +1,11 @@
+﻿using AvilokTaskAssignment.Api.DTO;
+
+namespace AvilokTaskAssignment.Api.Interfaces
+{
+    public interface ITaskCommentManager
+    {
+        Task<IEnumerable<CommentDto>> GetCommentsAsync(Guid taskId);
+
+        Task AddCommentAsync(Guid taskId, Guid authorId, string text);
+    }
+}
