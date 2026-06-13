bb5a8770123b0adc9e2c0f17014864f998e73363
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-13 10:00:24 +0200

    Create TaskComent.cs
    Vytvořena entitta TaskComent.cs.

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Data/Models/TaskComent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvilokTaskAssignment.Data.Models
+{
+    public class TaskComent
+    {
+        
+        public Guid Id { get; set; }
+
+        public Guid TaskId { get; set; }
+
+        public Guid AuthorId { get; set; }
+        public ApplicationUser Author { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
