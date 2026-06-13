608c6964f639c064c8988ccc60d5aed76417961c
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-14 01:28:19 +0200

    Create PermissionHelper.cs
    Vytvořená pomocná class,, ve které jsou vytvořeny metody pro hodně se opakující řádky v různých třídách.

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/Helpers/PermissionHelper.cs
@@ -0,0 +1,18 @@
+﻿using AvilokTaskAssignment.Data.Models;
+
+namespace AvilokTaskAssignment.Api.Helpers
+{
+    public static class PermissionHelper
+    {
+        public static bool HasTaskLeaderAccess(IEnumerable<string> roles, WorkType workType)
+        {
+            return roles.Contains("Admin") || roles.Contains(workType.GetLeaderRoleName());
+        }
+
+        public static void EnsureTaskLeaderAccess(IEnumerable<string> roles, WorkType workType, string errorMessage)
+        {
+            if (!HasTaskLeaderAccess(roles, workType))
+                throw new Exception(errorMessage);
+        }
+    }
+}
