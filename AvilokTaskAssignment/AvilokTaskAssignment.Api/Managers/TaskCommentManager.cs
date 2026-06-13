c292a306242f35e3b5827db1d3604a426b642e0b
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-13 11:25:47 +0200

    Create TaskCommentManager.cs
    Vytvořen TaskCommentManager.cs. Vlastní metody:
    
    GetCommentsAsync - vrátí všechny komentáře k danému úkolu.
    AddCommentAsync - Vytvoří nový komentář k úkolu

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/Managers/TaskCommentManager.cs
@@ -0,0 +1,58 @@
+﻿using AvilokTaskAssignment.Api.DTO;
+using AvilokTaskAssignment.Api.Interfaces;
+using AvilokTaskAssignment.Data.Interfaces;
+using AvilokTaskAssignment.Data.Models;
+
+namespace AvilokTaskAssignment.Api.Managers
+{
+    public class TaskCommentManager : ITaskCommentManager
+    {
+        private readonly ITaskCommentRepository _commentRepository;
+
+        public TaskCommentManager (ITaskCommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        #region Get
+        /// <summary>
+        /// Vrátí všechny komentáře k danému úkolu.
+        /// </summary>
+        public async Task<IEnumerable<CommentDto>> GetCommentsAsync(Guid taskId)
+        {
+            var comments = await _commentRepository.GetByTaskIdAsync(taskId);
+
+            return comments.Select(c => new CommentDto
+            {
+                Id = c.Id,
+                AuthorId = c.AuthorId,
+                AuthorName = c.Author.FullName,
+                Text = c.Text,
+                CreatedAt = c.CreatedAt
+            });
+        }
+    
+        #endregion
+
+        #region Post
+        /// <summary>
+        /// Přidá nový komentář k úkolu.
+        /// </summary>
+        public async Task AddCommentAsync(Guid taskId, Guid authorId, string text)
+        {
+            var comment = new TaskComment
+            {
+                Id = Guid.NewGuid(),
+                TaskId = taskId,
+                AuthorId = authorId,
+                Text = text,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await _commentRepository.AddAsync(comment);
+
+            await _commentRepository.SaveChangesAsync();
+        }
+        #endregion
+    }
+}
