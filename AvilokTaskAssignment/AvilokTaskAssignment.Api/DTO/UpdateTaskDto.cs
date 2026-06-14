e843a6c3e8cc3ff18a3b0d5d713f41f014af6af9
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-14 13:35:55 +0200

    Create UpdateTaskDto.cs
    Vytvořena class UpdateTaskDto.cs

---
+++ AvilokTaskAssignment/AvilokTaskAssignment.Api/DTO/UpdateTaskDto.cs
@@ -0,0 +1,15 @@
+﻿using AvilokTaskAssignment.Data.Models;
+
+namespace AvilokTaskAssignment.Api.DTO
+{
+    public class UpdateTaskDto
+    {
+        public string ShortDescription { get; set; } = string.Empty;
+
+        public string LongDescription {  get; set; } = string.Empty;
+
+        public WorkType WorkType {  get; set; }
+
+        public DateTime Deadline { get; set; }
+    }
+}
